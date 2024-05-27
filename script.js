b6b9497bf5712517d3fe22409bc4a06f4d5d85a0
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-27 21:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 
